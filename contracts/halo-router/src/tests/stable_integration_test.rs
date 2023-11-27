1efb8d1c351afe9a99a59e99d7e13d60d9939973
HaiTranQuang <haitranwang@gmail.com>
2023-11-27 14:02:21 +0700

    Update stable_integration_test.rs

--- contracts/halo-router/src/tests/stable_integration_test.rs
+++ contracts/halo-router/src/tests/stable_integration_test.rs
@@ -33,6 +33,8 @@ mod tests {
         };
         use std::str::FromStr;
 
+        use crate::tests::stable_env_setup::env::NATIVE_DENOM;
+
         use super::*;
 
         // Mock 1_000_000_000 USDC token amount
@@ -634,9 +636,9 @@ mod tests {
         // Create a stable pool with 1 native token and 2 cw20 tokens NATIVE_DENOM_2, USDT, BUSD
         // Provide liquidity to the stable pool (10000 NATIVE_DENOM_2, 20000 USDT, 30000 BUSD)
         // Create a pool NATIVE_DENOM, USDT
-        // Provide liquidity to the pool (10000 NATIVE_DENOM, 5000 USDT)
-        // ADMIN swap 100 NATIVE_DENOM to BUSD
-        // -> ADMIN should get approximately 50 BUSD
+        // Provide liquidity to the classic pool (10000 NATIVE_DENOM, 5000 USDT)
+        // ADMIN swap 100 NATIVE_DENOM_2 from Stable Pool to NATIVE_DENOM in Classic Pool
+        // -> ADMIN should get approximately 50 NATIVE_DENOM
         #[test]
         fn test_swap_native_stable_pair() {
             // get integration test app and contracts
@@ -692,7 +694,7 @@ mod tests {
 
             assert!(response.is_ok());
 
-            // add native token decimals
+            // add native token decimals for Stable Factory
             let add_native_token_decimals_msg = StableFactoryExecuteMsg::AddNativeTokenDecimals {
                 denom: NATIVE_DENOM_2.to_string(),
                 decimals: 6,
@@ -862,6 +864,310 @@ mod tests {
                     total_share: Uint128::zero(),
                 }
             );
+
+            // increase allowance for stable pool contract
+            let increase_allowance_msg = Cw20ExecuteMsg::IncreaseAllowance {
+                spender: create_stable_pair_response.contract_addr.clone(),
+                amount: Uint128::from(1_000_000_000u128 * ONE_UNIT_OF_DECIMAL_18),
+                expires: None,
+            };
+
+            // Execute increase allowance for USDT
+            let response = app.execute_contract(
+                Addr::unchecked(ADMIN.to_string()),
+                Addr::unchecked(usdt_token_contract.clone()),
+                &increase_allowance_msg,
+                &[],
+            );
+
+            assert!(response.is_ok());
+
+            // Execute increase allowance for BUSD
+            let response = app.execute_contract(
+                Addr::unchecked(ADMIN.to_string()),
+                Addr::unchecked(busd_token_contract.clone()),
+                &increase_allowance_msg,
+                &[],
+            );
+
+            assert!(response.is_ok());
+
+            // provide liquidity to the pool
+            let provide_liquidity_msg = StablePairExecuteMsg::ProvideLiquidity {
+                assets: vec![
+                    Asset {
+                        info: AssetInfo::NativeToken {
+                            denom: NATIVE_DENOM_2.to_string(),
+                        },
+                        amount: Uint128::from(10_000u128 * ONE_UNIT_OF_DECIMAL_6),
+                    },
+                    Asset {
+                        info: AssetInfo::Token {
+                            contract_addr: usdt_token_contract.clone(),
+                        },
+                        amount: Uint128::from(20_000u128 * ONE_UNIT_OF_DECIMAL_18),
+                    },
+                    Asset {
+                        info: AssetInfo::Token {
+                            contract_addr: busd_token_contract.clone(),
+                        },
+                        amount: Uint128::from(30_000u128 * ONE_UNIT_OF_DECIMAL_18),
+                    },
+                ],
+                slippage_tolerance: None,
+                receiver: None,
+            };
+
+            // Execute provide liquidity
+            let response = app.execute_contract(
+                Addr::unchecked(ADMIN.to_string()),
+                Addr::unchecked(create_stable_pair_response.contract_addr.to_string()),
+                &provide_liquidity_msg,
+                &[Coin {
+                    amount: Uint128::from(10_000u128 * ONE_UNIT_OF_DECIMAL_6),
+                    denom: NATIVE_DENOM_2.to_string(),
+                }],
+            );
+
+            assert!(response.is_ok());
+
+            // add native token decimals for Factory Contract
+            let add_native_token_decimals_msg = FactoryExecuteMsg::AddNativeTokenDecimals {
+                denom: NATIVE_DENOM.to_string(),
+                decimals: 6,
+            };
+
+            // Execute add native token decimals
+            let response = app.execute_contract(
+                Addr::unchecked(ADMIN.to_string()),
+                Addr::unchecked(halo_factory_contract.to_string()),
+                &add_native_token_decimals_msg,
+                &[Coin {
+                    amount: Uint128::from(MOCK_TRANSACTION_FEE),
+                    denom: NATIVE_DENOM.to_string(),
+                }],
+            );
+
+            assert!(response.is_ok());
+
+            // Create a pool NATIVE_DENOM, USDT
+            let asset_infos = [
+                AssetInfo::NativeToken {
+                    denom: NATIVE_DENOM.to_string(),
+                },
+                AssetInfo::Token {
+                    contract_addr: usdt_token_contract.clone(),
+                },
+            ];
+
+            // create pool msg
+            let create_pool_msg = HaloFactoryExecuteMsg::CreatePair {
+                asset_infos,
+                requirements: CreatePairRequirements {
+                    whitelist: vec![Addr::unchecked(ADMIN.to_string())],
+                    first_asset_minimum: Uint128::zero(),
+                    second_asset_minimum: Uint128::zero(),
+                },
+                commission_rate: Some(Decimal256::from_str("0.03").unwrap()),
+                lp_token_info: LPTokenInfo {
+                    lp_token_name: "aura-USDT".to_string(),
+                    lp_token_symbol: "aura-USDT".to_string(),
+                    lp_token_decimals: None,
+                },
+            };
+
+            // Execute create pool
+            let create_classic_pool_response = app.execute_contract(
+                Addr::unchecked(ADMIN.to_string()),
+                Addr::unchecked(halo_factory_contract.to_string()),
+                &create_pool_msg,
+                &[Coin {
+                    amount: Uint128::from(MOCK_TRANSACTION_FEE),
+                    denom: NATIVE_DENOM.to_string(),
+                }],
+            );
+
+            assert!(create_classic_pool_response.is_ok());
+
+            // query pool info
+            let create_classic_pool_response: PairInfo = app
+                .wrap()
+                .query_wasm_smart(
+                    Addr::unchecked(halo_factory_contract.to_string()),
+                    &HaloFactoryQueryMsg::Pair {
+                        asset_infos: [
+                            AssetInfo::NativeToken {
+                                denom: NATIVE_DENOM.to_string(),
+                            },
+                            AssetInfo::Token {
+                                contract_addr: usdt_token_contract.clone(),
+                            },
+                        ],
+                    },
+                )
+                .unwrap();
+
+            // Assert pool info
+            assert_eq!(
+                create_classic_pool_response,
+                PairInfo {
+                    contract_addr: create_classic_pool_response.contract_addr.clone(),
+                    liquidity_token: create_classic_pool_response.liquidity_token.clone(),
+                    asset_infos: [
+                        AssetInfo::NativeToken {
+                            denom: NATIVE_DENOM.to_string(),
+                        },
+                        AssetInfo::Token {
+                            contract_addr: usdt_token_contract.clone(),
+                        },
+                    ],
+                    asset_decimals: [6, 18],
+                    commission_rate: Decimal256::from_str("0.03").unwrap(),
+                    requirements: CreatePairRequirements {
+                        whitelist: vec![Addr::unchecked(ADMIN.to_string())],
+                        first_asset_minimum: Uint128::zero(),
+                        second_asset_minimum: Uint128::zero(),
+                    },
+                }
+            );
+
+            // increase allowance for pool contract for classic pool
+            let increase_allowance_msg = Cw20ExecuteMsg::IncreaseAllowance {
+                spender: create_classic_pool_response.contract_addr.clone(),
+                amount: Uint128::from(10_000u128 * ONE_UNIT_OF_DECIMAL_18),
+                expires: None,
+            };
+
+            // Execute increase allowance for USDT for classic pool
+            let response = app.execute_contract(
+                Addr::unchecked(ADMIN.to_string()),
+                Addr::unchecked(usdt_token_contract.clone()),
+                &increase_allowance_msg,
+                &[],
+            );
+
+            assert!(response.is_ok());
+
+            // Provide liquidity to the pool
+            let provide_liquidity_msg = ExecuteMsg::ProvideLiquidity {
+                assets: [
+                    Asset {
+                        info: AssetInfo::NativeToken {
+                            denom: NATIVE_DENOM.to_string(),
+                        },
+                        amount: Uint128::from(10_000u128 * ONE_UNIT_OF_DECIMAL_6),
+                    },
+                    Asset {
+                        info: AssetInfo::Token {
+                            contract_addr: usdt_token_contract.clone(),
+                        },
+                        amount: Uint128::from(5_000u128 * ONE_UNIT_OF_DECIMAL_18),
+                    },
+                ],
+                slippage_tolerance: None,
+                receiver: None,
+            };
+
+            // Execute provide liquidity
+            let response = app.execute_contract(
+                Addr::unchecked(ADMIN.to_string()),
+                Addr::unchecked(create_classic_pool_response.contract_addr),
+                &provide_liquidity_msg,
+                &[Coin {
+                    amount: Uint128::from(10_000u128 * ONE_UNIT_OF_DECIMAL_6),
+                    denom: NATIVE_DENOM.to_string(),
+                }],
+            );
+
+            assert!(response.is_ok());
+
+            // query balance of ADMIN in BUSD before swap
+            let req: QueryRequest<Cw20QueryMsg> = QueryRequest::Wasm(WasmQuery::Smart {
+                contract_addr: busd_token_contract.clone(),
+                msg: to_binary(&Cw20QueryMsg::Balance {
+                    address: ADMIN.to_string(),
+                })
+                .unwrap(),
+            });
+
+            let res = app.raw_query(&to_binary(&req).unwrap()).unwrap().unwrap();
+            let busd_balance_before_swap: BalanceResponse = from_binary(&res).unwrap();
+
+            // Assert balance of ADMIN in BUSD before swap
+            assert_eq!(
+                busd_balance_before_swap.balance,
+                Uint128::from(999_970_000_000_000_000_000_000_000u128),
+            );
+
+            // query balance of ADMIN in NATIVE_DENOM before swap
+            let req: QueryRequest<BankQuery> = QueryRequest::Bank(BankQuery::Balance {
+                address: ADMIN.to_string(),
+                denom: NATIVE_DENOM.to_string(),
+            });
+
+            let res = app.raw_query(&to_binary(&req).unwrap()).unwrap().unwrap();
+            let denom_2_balance_before_swap: BankBalanceResponse = from_binary(&res).unwrap();
+
+            // Assert balance of ADMIN in NATIVE_DENOM before swap
+            assert_eq!(
+                denom_2_balance_before_swap.amount.amount,
+                Uint128::from(989999990000u128),
+            );
+
+            // Swap 100 NATIVE_DENOM_2 to NATIVE_DENOM via router with operation HaloStableSwap(NATIVE_DENOM_2 -> USDT) and HaloSwap(USDT -> NATIVE_DENOM)
+            let swap_msg = RouterExecuteMsg::ExecuteSwapOperations {
+                operations: vec![
+                    SwapOperation::StableSwap {
+                        offer_asset_info: AssetInfo::NativeToken {
+                            denom: NATIVE_DENOM_2.to_string(),
+                        },
+                        ask_asset_info: AssetInfo::Token {
+                            contract_addr: usdt_token_contract.clone(),
+                        },
+                        asset_infos: vec![
+                            AssetInfo::NativeToken {
+                                denom: NATIVE_DENOM_2.to_string(),
+                            },
+                            AssetInfo::Token {
+                                contract_addr: usdt_token_contract.clone(),
+                            },
+                            AssetInfo::Token {
+                                contract_addr: busd_token_contract.clone(),
+                            },
+                        ],
+                    },
+                    SwapOperation::HaloSwap {
+                        offer_asset_info: AssetInfo::Token {
+                            contract_addr: usdt_token_contract.clone(),
+                        },
+                        ask_asset_info: AssetInfo::NativeToken {
+                            denom: NATIVE_DENOM.to_string(),
+                        },
+                    },
+                ],
+                minimum_receive: None,
+                to: None,
+            };
+
+            // Execute swap
+            let response = app.execute_contract(
+                Addr::unchecked(ADMIN.to_string()),
+                Addr::unchecked(router_contract.to_string()),
+                &swap_msg,
+                &[Coin {
+                    amount: Uint128::from(100u128 * ONE_UNIT_OF_DECIMAL_6),
+                    denom: NATIVE_DENOM_2.to_string(),
+                }],
+            );
+
+            assert!(response.is_ok());
+
+
+
+
+
+
+
         }
     }
 }
